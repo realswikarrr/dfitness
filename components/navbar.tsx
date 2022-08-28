67c16a6255c038b7d1d8f46da862c52a6146e213
realswikarrr <swikara082@gmail.com>
2022-08-28 23:31:42 +0545

    feat: 🛠 Added Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,22 +1,24 @@
+import Link from "next/link";
+import { AiFillHome } from "react-icons/ai";
+import { BsPersonFill, BsFillCartFill } from "react-icons/bs";
+
 /* eslint-disable @next/next/no-img-element */
 const NavBar = () => {
   return (
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+    <nav className="bg-black border-gray-200 px-2 sm:px-4 py-2.5 ">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+        <Link href="" className="flex items-center">
+          <span className="self-center text-red-600 text-3xl font-semibold whitespace-nowrap ">
+            Diamond
+            <span className="self-center text-3xl text-white ml-1  font-semibold whitespace-nowrap ">
+              Fitness
+            </span>
           </span>
-        </a>
+        </Link>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 ml-3  text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none  "
           aria-controls="navbar-default"
           aria-expanded="false"
         >
@@ -35,48 +37,28 @@ const NavBar = () => {
             ></path>
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                aria-current="page"
-              >
-                Home
-              </a>
+        <div id="navbar-default">
+          <ul className="flex flex-row space-x-6 text-[16px] font-semibold  text-white  ">
+            <li className="hover:bg-red-600 p-4 cursor-pointer">
+              <Link href="#">
+                <span className="flex items-center">
+                  <AiFillHome className="mr-1" /> Home
+                </span>
+              </Link>
             </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                About
-              </a>
+            <li className="hover:bg-red-600 p-4 cursor-pointer">
+              <Link href="#">
+                <span className="flex items-center">
+                  <BsPersonFill className="mr-1" /> Sign In
+                </span>
+              </Link>
             </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </a>
+            <li className="hover:bg-red-600 p-4 cursor-pointer">
+              <Link href="#">
+                <span className="flex items-center">
+                  <BsFillCartFill className="mr-1" /> Cart
+                </span>
+              </Link>
             </li>
           </ul>
         </div>
